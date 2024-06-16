aca54f5c9641c07916d9288fba4b57562aa60a03
arianerubin <165211589+arianerubin@users.noreply.github.com>
2024-06-16 12:23:06 -0400

    emojipediapracticepropsmaps

---
+++ src/emojipedia.js
@@ -0,0 +1,49 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym.",
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you.",
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“.",
+  },
+
+  {
+    id: 4,
+    emoji: "😍️",
+    name: "Smiling Face With Heart-Eyes",
+    meaning:
+      "A smile of love is shown by the 😍 emoji, which beams with happiness with its heart-shaped eyes. It conveys intense love and affection for a person, place or even an object. This emoji is often used in romantic messages to express love, gratitude, and admiration. It can also be used to convey enthusiasm for a cause or event. The heart-shaped eyes symbolize overwhelming feelings of happiness and a strong connection.",
+  },
+
+  {
+    id: 5,
+    emoji: "🥳️",
+    name: "Face with party blower and party hat",
+    meaning:
+      "That was a wild party“ or “There's something to celebrate“. For pure party vibes, try the 🥳 emoji: a face in a party hat blowing a horn as confetti flies around its head. It symbolizes the joy of exuberant celebrations and happy occasions like New Year's Eve, birthdays, or World Emoji Day. This emoji expresses that there is something to celebrate or that you have had a wild party. Whatever the occasion, this emoji emphasizes the celebration. It shows that you are ready to enjoy an event to the fullest and look forward to what is to come. Additionally, it can stand for carnival and express the joy of the fifth season.",
+  },
+
+  {
+    id: 6,
+    emoji: "😭️",
+    name: "Loudly Crying Face",
+    meaning:
+      "A face crying violently 😭 symbolizes despair and inconsolability, while the tears flow from the eyes like a waterfall. It represents feelings of sadness, pain, or defeat, and is sometimes used in ironic contexts. For example, when something is so funny that you can hardly stop yourself from laughing, or when someone is making a drama out of an insignificant situation. This emoji is one of the most popular on social media sites like Twitter and expresses some intense emotions like pride, overwhelming joy or even relief. ",
+  },
+];
+
+export default emojipedia;
